8c9e9438341ef7f917f4c434ee4160562097309a
lunaplush <lunaplus@mail.ru>
2018-04-13 16:45:59 +0300

    example simple face detector

--- main.py
+++ main.py
@@ -20,3 +20,98 @@ ax[0].im show(image,cmap='gray')
 ax[0].set_title('input image')
 ax[1].imshow(hog_vis)
 ax[1].set_title('visualisation of HOG features')
+#%% получим положительные примеры
+
+from sklearn.datasets import fetch_lfw_people
+faces = fetch_lfw_people()
+positive_patches = faces.images
+positive_patches.shape
+
+#%% получим отрицательные примеры
+from skimage import data, transform
+
+imgs_to_use = ['camera', 'text', 'coins', 'moon',
+               'page', 'clock', 'immunohistochemistry',
+               'chelsea', 'coffee', 'hubble_deep_field']
+images = [color.rgb2gray(getattr(data, name)()) for name in imgs_to_use]
+
+from sklearn.feature_extraction.image import PatchExtractor
+
+def extract_patches(img, N, scale=1.0, patch_size=positive_patches[0].shape):
+    extracted_patch_size = tuple((scale * np.array(patch_size)).astype(int))
+    extractor = PatchExtractor(patch_size=extracted_patch_size,
+                               max_patches=N, random_state=0)
+    patches = extractor.transform(img[np.newaxis])
+    if scale != 1:
+        patches = np.array([transform.resize(patch, patch_size)
+                            for patch in patches])
+    return patches
+
+negative_patches = np.vstack([extract_patches(im, 1000, scale)
+                              for im in images for scale in [0.5, 1.0, 2.0]])
+negative_patches.shape
+#%%  просмотр отрицательных примеров
+fig, ax = plt.subplots(6, 10)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(negative_patches[500 * i], cmap='gray')
+    axi.axis('off')
+#%%     сформируем обучающую выборку скомбинировав положительные и отрицательные примеры
+from itertools import chain
+X_train = np.array([feature.hog(im)
+                    for im in chain(positive_patches,
+                                    negative_patches)])
+y_train = np.zeros(X_train.shape[0])
+y_train[:positive_patches.shape[0]] = 1
+
+X_train.shape
+
+#%% обучим линейную машину опорных векторов на сформированной выборке
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cross_validation import cross_val_score
+
+cross_val_score(GaussianNB(), X_train, y_train)
+
+from sklearn.svm import LinearSVC
+from sklearn.grid_search import GridSearchCV
+grid = GridSearchCV(LinearSVC(), {'C': [1.0, 2.0, 4.0, 8.0]})
+grid.fit(X_train, y_train)
+grid.best_score_
+grid.best_params_
+model = grid.best_estimator_
+model.fit(X_train, y_train)
+
+#%% Применим построенную модель для поиска лица на изображении астронавта
+test_image = skimage.data.astronaut()
+test_image = skimage.color.rgb2gray(test_image)
+test_image = skimage.transform.rescale(test_image, 0.5)
+test_image = test_image[:160, 40:180]
+
+plt.imshow(test_image, cmap='gray')
+plt.axis('off');
+
+def sliding_window(img, patch_size=positive_patches[0].shape,
+                   istep=2, jstep=2, scale=1.0):
+    Ni, Nj = (int(scale * s) for s in patch_size)
+    for i in range(0, img.shape[0] - Ni, istep):
+        for j in range(0, img.shape[1] - Ni, jstep):
+            patch = img[i:i + Ni, j:j + Nj]
+            if scale != 1:
+                patch = transform.resize(patch, patch_size)
+            yield (i, j), patch
+            
+indices, patches = zip(*sliding_window(test_image))
+patches_hog = np.array([feature.hog(patch) for patch in patches])
+patches_hog.shape
+labels = model.predict(patches_hog)
+labels.sum()
+
+fig, ax = plt.subplots()
+ax.imshow(test_image, cmap='gray')
+ax.axis('off')
+
+Ni, Nj = positive_patches[0].shape
+indices = np.array(indices)
+
+for i, j in indices[labels == 1]:
+    ax.add_patch(plt.Rectangle((j, i), Nj, Ni, edgecolor='red',
+                               alpha=0.3, lw=2, facecolor='none'))
